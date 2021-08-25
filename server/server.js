f6ea2b47ae950ee3e297e73846e08e8f2748a0de
Liz <thompsonel2011@gmail.com>
2021-08-25 12:36:11 -0400

    file structure

--- server/server.js
+++ server/server.js
@@ -7,7 +7,6 @@ const PORT = process.env.PORT || 3001;
 
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
-const { authMiddleware } = require('./utils/auth');
 
 
 const server = new ApolloServer({
